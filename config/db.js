7a20d467e9cf718cedc893bd31994f428e9eee44
Anubhavchaudhary011 <142311561+Anubhavchaudhary011@users.noreply.github.com>
2025-10-10 21:45:15 +0530

    Update db.js

--- config/db.js
+++ config/db.js
@@ -1,6 +1,6 @@
 import mongoose from"mongoose"
 export const connectdb=async()=>{
- await mongoose.connect('mongodb+srv://anubhav:KnVhdRa9gnVO2jB1@cluster0.8jzyu64.mongodb.net/?retryWrites=true&w=majority&appName=supreme').then(()=>{
+ await mongoose.connect('').then(()=>{
         console.log("db connected");
     });
 }
